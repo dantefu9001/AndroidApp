4f0c3417363f402d477a1c9606c0a93e1e8b4780
zenan <zenan.fu@thoughtworks.com>
2020-04-22 09:08:37 +0800

    initial commit

---
+++ app/src/main/java/com/zn/thirdapp/activity/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.zn.thirdapp.activity
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.zn.thirdapp.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        //sms and dial intents
+        sms.setOnClickListener {
+            val smsToUri = Uri.parse("smsto:")
+            val intent = Intent(Intent.ACTION_SENDTO, smsToUri)
+            startActivity(intent)
+        }
+        dial.setOnClickListener {
+            val intent = Intent(Intent.ACTION_DIAL)
+            startActivity(intent)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
